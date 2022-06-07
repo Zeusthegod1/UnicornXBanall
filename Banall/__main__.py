567fea1f642ccd2dbba144ebcf86ba7994fe2a07
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-07 16:34:34 +0530

    Check

--- Banall/__main__.py
+++ Banall/__main__.py
@@ -4,24 +4,30 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from . import bot 
 from Banall import STARTED, FINISH, ERROR, OWN_UNAME
 
+@bot.on_message(filters.group)
+def NewChat(bot,message):
+    logging.info("new chat {}".format(message.chat.id))
+    logging.info("getting memebers from {}".format(message.chat.id))
+    a= bot.iter_chat_members(message.chat.id)
+
 
 @bot.on_message(filters.group & filters.command("banall"))
 def main(_, msg: Message):
     chat = msg.chat
     me = chat.get_member(bot.get_me().id)
-    if chat.get_member(msg.from_user.id).can_manage_chat and me.can_restrict_members and me.can_delete_messages:
+    for i in a:
         try:
             msg.reply(STARTED.format(chat.members_count))
             count_kicks = 0
             for member in chat.iter_members():
                 if not member.can_manage_chat:
-                    bot.ban_chat_member(chat_id=msg.chat.id, user_id=member.user.id)
+                    bot.kick_chat_member(chat_id=msg.chat.id, user_id=i.user.id)
                     count_kicks += 1
             msg.reply(FINISH.format(count_kicks))
         except Exception as e:
             msg.reply(ERROR.format(str(e)))
     else:
-        msg.reply("i need to be admin In This Group To Perform This Action!")
+        msg.reply(OWN_UNAME, "i need to be admin In This Group To Perform This Action!")
 
 
 @bot.on_message(filters.group & filters.service, group=2)
